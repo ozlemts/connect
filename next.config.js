b217a7bc134a189b30392959fe7e5ea6981f9e53
ozlemts <ozlemturkerii@gmail.com>
2021-10-24 14:12:41 +0200

    webpack error fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     if (!isServer) {
       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
       config.resolve.fallback = {
-        fs: false
+        fs: true
       }
     }
 
