a0ffa82bd0864f6b75900b32404b78834eb66ee8
ozlemts <ozlemturkerii@gmail.com>
2021-10-24 14:07:16 +0200

    webpack 5 fix for build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  future: {
-    webpack5: false
-  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false
+      }
+    }
+
+    return config;
+  }
 }
